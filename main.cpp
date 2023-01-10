972bb7ba6982a61b0e626ca899d3bd19fb004798
Nick <20363253+zoidq@users.noreply.github.com>
2023-01-10 21:02:14 +0000

    Open window

--- main.cpp
+++ main.cpp
@@ -3,8 +3,39 @@
 
 int main()
 {
+    std::cout << "Starting.." << std::endl;
 
-  return 0;
+    if(SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        std::cout << "Failed to initialize the SDL2 library\n";
+        return -1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow("SDL2 Window",
+            SDL_WINDOWPOS_CENTERED,
+            SDL_WINDOWPOS_CENTERED,
+            680, 480,
+            0);
+
+    if(!window)
+    {
+        std::cout << "Failed to create window\n";
+        return -1;
+    }
+
+    SDL_Surface *window_surface = SDL_GetWindowSurface(window);
+
+    if(!window_surface)
+    {
+        std::cout << "Failed to get the surface from the window\n";
+        return -1;
+    }
+
+    SDL_UpdateWindowSurface(window);
+
+    SDL_Delay(5000);
+
+    return 0;
 }
 
 
